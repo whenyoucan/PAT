bfdee5eab377df2ddf67a86a304602c479731505
whenyoucan <wengyukan@gmail.com>
2014-10-14 20:44:45 +0800

    optimize

--- Basic/1005.py
+++ Basic/1005.py
@@ -1,13 +1,11 @@
-n = raw_input()
-num_list = [int(n) for n in raw_input().split()]
-for n in num_list[:]:
+raw_input()
+nums = {int(i) for i in raw_input().split()}
+for n in nums.copy():
     while n > 1:
         if n % 2 == 0:
             n /= 2
         else:
             n = (3 * n + 1) / 2
-        while n in num_list:
-            num_list.remove(n)
-num_list.sort(reverse=True)
-for n in num_list:
-    print n,
+        if n in nums:
+            nums.remove(n)
+print ' '.join([str(i) for i in sorted(list(nums), reverse=True)])
