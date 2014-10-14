368dd0ba7dd2a54ebd8bf4273b97d5093143c8a2
whenyoucan <wengyukan@gmail.com>
2014-10-15 00:09:22 +0800

    optimize

--- Basic/1010.py
+++ Basic/1010.py
@@ -1,8 +1,7 @@
-n = [int(i) for i in raw_input().split()]
-flag = False
-for i in range(0, len(n), 2):
-    if n[i + 1] != 0:
-        flag = True
-        print n[i] * n[i + 1], n[i + 1] - 1,
-if not flag:
-    print 0, 0,
+nums = [int(i) for i in raw_input().split()]
+old_pairs = zip(*[iter(nums)] * 2)  # [3, 4, -5, 2, 6, 1, -2, 0] -> [(3, 4), (-5, 2), (6, 1), (-2, 0)]
+new_pairs = [(i[0] * i[1], i[1] - 1) for i in old_pairs if i[0] * i[1] != 0]
+if len(new_pairs) > 0:
+    print ' '.join(['{} {}'.format(pair[0], pair[1]) for pair in new_pairs])
+else:
+    print '0 0'
