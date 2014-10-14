22021da62a26dfb79d29c2a035cfdfb5ebc3241c
whenyoucan <wengyukan@gmail.com>
2014-10-15 00:14:26 +0800

    optimize

--- Basic/1011.py
+++ Basic/1011.py
@@ -1,6 +1,6 @@
 for i in xrange(int(raw_input())):
-    nums = [int(i) for i in raw_input().split()]
+    nums = [int(n) for n in raw_input().split()]
     if nums[0] + nums[1] > nums[2]:
-        print "Case #%d: true" % (i + 1)
+        print "Case #{}: true".format(i + 1)
     else:
-        print "Case #%d: false" % (i + 1)
+        print "Case #{}: false".format(i + 1)
