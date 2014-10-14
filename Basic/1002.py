bc60fd4e0b445e31aba7d1ee068a091ea55c8fc4
whenyoucan <wengyukan@gmail.com>
2014-10-14 20:05:43 +0800

    optimize

--- Basic/1002.py
+++ Basic/1002.py
@@ -1,16 +1,12 @@
-num_str = raw_input()
-num_lst = [int(c) for c in num_str]
-sum_num = sum(num_lst)
-sum_str = str(sum_num)
-map_digit_char_to_pinyin = {'0': 'ling',
-                            '1': 'yi',
-                            '2': 'er',
-                            '3': 'san',
-                            '4': 'si',
-                            '5': 'wu',
-                            '6': 'liu',
-                            '7': 'qi',
-                            '8': 'ba',
-                            '9': 'jiu'}
-for digit_char in sum_str:
-    print map_digit_char_to_pinyin[digit_char],
+sum_str = str(sum([int(i) for i in raw_input()]))
+map_digit_to_pinyin = {'0': 'ling',
+                       '1': 'yi',
+                       '2': 'er',
+                       '3': 'san',
+                       '4': 'si',
+                       '5': 'wu',
+                       '6': 'liu',
+                       '7': 'qi',
+                       '8': 'ba',
+                       '9': 'jiu'}
+print ' '.join([map_digit_to_pinyin[digit] for digit in sum_str])
