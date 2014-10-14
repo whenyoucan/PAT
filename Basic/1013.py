45e02723742096ce6f1a6032ba01786508b95829
whenyoucan <wengyukan@gmail.com>
2014-10-14 21:30:07 +0800

    optimize

--- Basic/1013.py
+++ Basic/1013.py
@@ -1,24 +1,13 @@
-def primes(n):
-    p = [True] * (n + 1)
-    for i in xrange(2, int(n ** 0.5) + 1):
+def list_primes(the_range):
+    p = [True] * (the_range + 1)
+    for i in xrange(2, int(the_range ** 0.5) + 1):
         if p[i]:
-            p[i + i::i] = [False] * (n / i - 1)
-    return [i for i in xrange(2, n + 1) if p[i]]
+            p[i + i::i] = [False] * (the_range / i - 1)
+    return [i for i in xrange(2, the_range + 1) if p[i]]
 
-# def primes(n):
-#     p = [1] * (n / 2)
-#     for i in xrange(3, int(n ** 0.5) + 1, 2):
-#         if p[i / 2]:
-#             p[i * i / 2::i] = [0] * ((n - i * i - 1) / (2 * i) + 1)
-#     return [2] + [2 * i + 1 for i in xrange(1, n / 2) if p[i]]
 
-prime_list = primes(150000)
-nums = [int(i) for i in raw_input().split()]
-m, n, count = nums[0], nums[1], 0
-for i in xrange(m, n + 1):
-    count += 1
-    if count == 10:
-        count = 0
-        print prime_list[i - 1]
-    else:
-        print prime_list[i - 1],
+m, n = [int(i) for i in raw_input().split()]
+primes = list_primes(150000)[m - 1:n]
+
+for idx in xrange(0, len(primes), 10):
+    print ' '.join([str(i) for i in primes[idx:idx + 10]])
