805d71c8904af9cc84e9e1466475cfca8bd47250
whenyoucan <wengyukan@gmail.com>
2014-10-15 20:24:14 +0800

    optimize

--- Basic/1023.py
+++ Basic/1023.py
@@ -1,8 +1,5 @@
 nums = [int(i) for i in raw_input().split()]
-s = []
-for i in xrange(10):
-    s += [str(i)] * nums[i]
+s = ''.join([str(i) * nums[i] for i in xrange(10)])
 first_non_zero = filter(lambda x: x != '0', s)[0]
-s.remove(first_non_zero)
-s = first_non_zero + ''.join(s)
+s = first_non_zero + s.replace(first_non_zero, '', 1)
 print s
