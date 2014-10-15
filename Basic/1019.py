4ffd3e57085de7f1dab206c4924babee85037be4
whenyoucan <wengyukan@gmail.com>
2014-10-15 17:46:13 +0800

    optimize

--- Basic/1019.py
+++ Basic/1019.py
@@ -1,9 +1,9 @@
-n = raw_input().rjust(4, '0')
+n = '{:0>4}'.format(raw_input())
 while True:
     a = int(''.join(sorted(n, reverse=True)))
     b = int(''.join(sorted(n)))
     result = a - b
-    print "%04d - %04d = %04d" % (a, b, result)
+    print "{:0>4} - {:0>4} = {:0>4}".format(a, b, result)
     if result == 0 or result == 6174:
         break
-    n = "%04d" % result
+    n = '{:0>4}'.format(result)
