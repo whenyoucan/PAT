bf86247bfe9cc29261055ba3d76d6213ccd32a53
whenyoucan <wengyukan@gmail.com>
2014-10-15 23:25:52 +0800

    optimize

--- Basic/1024.py
+++ Basic/1024.py
@@ -1,18 +1,13 @@
-original = raw_input().split('E')
-real = original[0]
-expo = int(original[1])
-positive = real[0] == '+'
-real = [i for i in real[1:]]
-real.remove('.')
-if expo > 0:
-    if len(real) <= expo + 1:
-        real += ['0'] * (expo + 1 - len(real))
-    else:
-        real.insert(1 + expo, '.')
-else:
-    real = ['0'] * (-expo) + real
-    real.insert(1, '.')
-real = ''.join(real)
-if not positive:
-    real = '-' + real
-print real
+num_str = raw_input()
+flag = num_str[0]
+coe_str, exp_str = num_str[1:].split('E')
+l_1 = len(coe_str.split('.')[1])
+l_2 = int(exp_str)
+coe_str = coe_str.replace('.', '')
+if l_2 < 0:  # e.g:-1.234E-2
+    result = '0.' + '0' * (-l_2 - 1) + coe_str
+elif l_2 < l_1:  # e.g: -1.234E+2
+    result = coe_str[:l_2 - l_1] + '.' + coe_str[l_2 - l_1:]
+else:  # e.g: -1.2E+10
+    result = coe_str + '0' * (l_2 - l_1)
+print '{f}{num}'.format(f=flag if flag == '-' else '', num=result)
