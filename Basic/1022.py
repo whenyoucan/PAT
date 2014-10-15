ed02d3c9938e94d696ff8b0261e55089e5188169
whenyoucan <wengyukan@gmail.com>
2014-10-15 17:57:35 +0800

    optimize

--- Basic/1022.py
+++ Basic/1022.py
@@ -1,11 +1,10 @@
 args = [int(i) for i in raw_input().split()]
 n, d = args[0] + args[1], args[2]
 result = ''
-if n == 0:
-    print '0'
-    exit()
-else:
+if n:
     while n != 0:
         result = str(n % d) + result
         n /= d
+else:
+    result = '0'
 print result
