eddfdee2fc53e9cb5abfc84100c9c5b2ea0cfda9
whenyoucan <wengyukan@gmail.com>
2014-10-15 23:29:42 +0800

    optimize

--- Basic/1024.py
+++ Basic/1024.py
@@ -1,13 +1,13 @@
 num_str = raw_input()
 flag = num_str[0]
 coe_str, exp_str = num_str[1:].split('E')
-l_1 = len(coe_str.split('.')[1])
-l_2 = int(exp_str)
+offset = int(exp_str)
+fraction_len = len(coe_str.split('.')[1])
 coe_str = coe_str.replace('.', '')
-if l_2 < 0:  # e.g:-1.234E-2
-    result = '0.' + '0' * (-l_2 - 1) + coe_str
-elif l_2 < l_1:  # e.g: -1.234E+2
-    result = coe_str[:l_2 - l_1] + '.' + coe_str[l_2 - l_1:]
+if offset < 0:  # e.g:-1.234E-2
+    result = '0.' + '0' * (-offset - 1) + coe_str
+elif offset < fraction_len:  # e.g: -1.234E+2
+    result = coe_str[:offset - fraction_len] + '.' + coe_str[offset - fraction_len:]
 else:  # e.g: -1.2E+10
-    result = coe_str + '0' * (l_2 - l_1)
+    result = coe_str + '0' * (offset - fraction_len)
 print '{f}{num}'.format(f=flag if flag == '-' else '', num=result)
