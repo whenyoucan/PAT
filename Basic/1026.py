51d2aa31cd16447621fd32cb52fb165eae6a6e09
whenyoucan <wengyukan@gmail.com>
2014-10-16 11:05:55 +0800

    optimize

--- Basic/1026.py
+++ Basic/1026.py
@@ -1,13 +1,4 @@
-def calc_hhmmss(second):
-    hh = second / 3600
-    second %= 3600
-    mm = second / 60
-    second %= 60
-    ss = second
-    return hh, mm, ss
-
-
 c1, c2 = raw_input().split()
 c = int(c2) - int(c1)
 c = int(c / 100.0 + 0.5)
-print ':'.join(str(i).rjust(2, '0') for i in calc_hhmmss(c))
+print '{:0>2}:{:0>2}:{:0>2}'.format(c / 3600, c % 3600 / 60, c % 60)
