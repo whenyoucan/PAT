0058bf761535fc5eabc6d44ddb78720de3f6397c
whenyoucan <wengyukan@gmail.com>
2014-10-16 12:15:58 +0800

    optimize

--- Basic/1027.py
+++ Basic/1027.py
@@ -1,10 +1,5 @@
-sizes = [n * n * 2 - 1 for n in xrange(0, 24)]
-N, c = raw_input().split()
-N = int(N)
-for i in xrange(1, len(sizes)):
-    if N <= sizes[i + 1]:
-        break
-level = i
-for i in range(level, 0, -1) + range(2, level + 1, 1):
-    print ''.join([' ' * (level - i), c * (2 * i - 1)])
-print N - sizes[level]
+n, c = raw_input().split()
+level = int(((int(n) + 1) / 2) ** 0.5)
+for l in range(level, 0, -1) + range(2, level + 1):
+    print '{spaces}{chars}'.format(spaces=' ' * (level - l), chars=c * (2 * l - 1))
+print int(n) - (level * level * 2 - 1)
