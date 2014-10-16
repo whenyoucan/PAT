14d39eac0ee745eabee1652fd069e5674e55caf3
whenyoucan <wengyukan@gmail.com>
2014-10-16 10:42:49 +0800

    optimize

--- Basic/1025.py
+++ Basic/1025.py
@@ -1,19 +1,16 @@
-cur_addr, n, k = raw_input().split()
-n, k = int(n), int(k)
+cur_addr, n, k = [int(i) for i in raw_input().split()]
 d = {}
 for i in xrange(int(n)):
-    addr, data, nxt = raw_input().split()
-    d[addr] = {'addr': addr, 'data': data, 'next': nxt}
-l = []
-while cur_addr != '-1':
-    l.append(d[cur_addr])
-    cur_addr = d[cur_addr]['next']
-for idx in xrange(0, len(l) - len(l) % k, k):
-    l[idx:idx + k] = l[idx:idx + k][::-1]
-for idx in xrange(0, len(l)):
-    if idx + 1 < len(l):
-        l[idx]['next'] = l[idx + 1]['addr']
+    addr, data, nxt = [int(i) for i in raw_input().split()]
+    d[addr] = [data, nxt]
+order = []
+while cur_addr != -1:
+    order.append(cur_addr)
+    cur_addr = d[cur_addr][1]
+for i in xrange(0, len(order) - len(order) % k, k):
+    order[i:i + k] = order[i:i + k][::-1]
+for i in xrange(0, len(order)):
+    if i + 1 < len(order):
+        print '{:0>5} {} {:0>5}'.format(order[i], d[order[i]][0], order[i + 1])
     else:
-        l[idx]['next'] = '-1'
-for i in l:
-    print '{} {} {}'.format(i['addr'], i['data'], i['next'])
+        print '{:0>5} {} -1'.format(order[i], d[order[i]][0])
