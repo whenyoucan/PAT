228d14c972bd11cb0bb462e3bc3f49adfe475ccd
whenyoucan <wengyukan@gmail.com>
2014-11-04 10:05:05 +0800

    e.g.

--- Basic/1024.py
+++ Basic/1024.py
@@ -4,10 +4,10 @@ coe_str, exp_str = num_str[1:].split('E')
 offset = int(exp_str)
 fraction_len = len(coe_str.split('.')[1])
 coe_str = coe_str.replace('.', '')
-if offset < 0:  # e.g:-1.234E-2
+if offset < 0:  # e.g. -1.234E-2
     result = '0.' + '0' * (-offset - 1) + coe_str
-elif offset < fraction_len:  # e.g: -1.234E+2
+elif offset < fraction_len:  # e.g. -1.234E+2
     result = coe_str[:offset - fraction_len] + '.' + coe_str[offset - fraction_len:]
-else:  # e.g: -1.2E+10
+else:  # e.g. -1.2E+10
     result = coe_str + '0' * (offset - fraction_len)
 print '{f}{num}'.format(f=flag if flag == '-' else '', num=result)
