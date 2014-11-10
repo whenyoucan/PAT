fd0876ad2f2fcae72418df99de9ecad584b87749
whenyoucan <wengyukan@gmail.com>
2014-11-10 17:09:49 +0800

    optimize

--- Advanced/1005.py
+++ Advanced/1005.py
@@ -1,3 +1,3 @@
-n = map(int, str(sum(map(int, list(raw_input())))))
-for i in n:
-    print ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'][i],
+n = str(sum(map(int, list(raw_input()))))
+for c in n:
+    print ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'][int(c)],
