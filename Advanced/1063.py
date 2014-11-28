dcd0e9fa71a076b7bbdade2b7d9cfba0fc89535f
whenyoucan <wengyukan@gmail.com>
2014-11-28 16:53:20 +0800

    optimize

--- Advanced/1063.py
+++ Advanced/1063.py
@@ -4,4 +4,5 @@ for i in xrange(n):
     sets.append(set([int(i) for i in raw_input().split()][1:]))
 for i in xrange(input()):
     a, b = [int(i) - 1 for i in raw_input().split()]
-    print '{:.1f}%'.format(len(sets[a].intersection(sets[b])) * 100.0 / len(sets[a].union(sets[b])))
+    common = len(sets[a].intersection(sets[b]))
+    print '{:.1f}%'.format(common * 100.0 / (len(sets[a]) + len(sets[b]) - common))
